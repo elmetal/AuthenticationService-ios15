6fd4947af9fa990267f093ca977c3b107727391f
elmetal <nm7.pupu@gmail.com>
2021-09-03 17:03:59 +0900

    AuthenticationSession was compiled with optimization error

--- AuthenticationService-ios15/AuthenticationSession/AuthenticationSessionModel.swift
+++ AuthenticationService-ios15/AuthenticationSession/AuthenticationSessionModel.swift
@@ -8,18 +8,24 @@
 
 import Foundation
 
-public struct AuthenticationSessionModel<AS: AuthenticationSessionProtocol> {
+public class AuthenticationSessionModel<AS: AuthenticationSessionProtocol> {
+    private var authenticationSession: AS?
+
     public init() {}
 
     public func action() {
+        guard authenticationSession == nil else {
+            return
+        }
+
         guard let authURL = URL(string: "https://example.com/auth") else { return }
         let scheme = "exampleauth"
 
-        let authenticationSession = AS.init(url: authURL, callbackURLScheme: scheme)
+        let authenticationSession2 = AS.init(url: authURL, callbackURLScheme: scheme)
         { callbackURL, error in
             // Handle the callback.
         }
 
-        authenticationSession.sessionStart()
+        authenticationSession2.sessionStart()
     }
 }
